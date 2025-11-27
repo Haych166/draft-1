82fe483de1a3e7fac3380556441a80c82c69fcd8
Haych166 <Henryt.thomas@yahoo.com>
2025-11-27 12:19:10 +0000

    Create livechat.jsx

---
+++ components/livechat.jsx
@@ -0,0 +1,9 @@
+export function LiveChat() {
+  return (
+    <div className="fixed bottom-4 right-4">
+      <button className="px-3 py-2 rounded-full bg-sky-400 text-slate-950 text-xs font-semibold shadow-lg">
+        Chat (Coming Soon)
+      </button>
+    </div>
+  );
+}
